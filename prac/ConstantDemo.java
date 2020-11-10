79c17bb31a72c439b4809b1993df24649ee0be78
taojian <jian.tao01@liulishuo.com>
2020-11-10 08:23:37 +0800

    常量

---
+++ prac/ConstantDemo.java
@@ -0,0 +1,13 @@
+
+
+public class ConstantDemo {
+    public static void main(String[] args) {
+        System.out.println("string");
+        System.out.println(12);
+        System.out.println(12.3);
+        System.out.println('q');
+        System.out.println(true);
+        // 空常量不能直接输出
+        // System.out.println(null);
+    }
+}
